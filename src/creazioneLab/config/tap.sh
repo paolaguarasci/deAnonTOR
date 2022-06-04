d2bbdcf259501f9e361ddcb8a8a6f8f235db8540
Paola Guarasci <paolaguarasci@gmail.com>
2022-06-04 13:55:02 +0200

    config router

---
+++ src/creazioneLab/config/tap.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+ 
+sudo tunctl -t tap0
+
+sudo ifconfig tap0 192.168.122.1
+sudo ifconfig tap0 netmask 255.255.255.252
+sudo ifconfig tap0 broadcast 192.168.122.3
+sudo ifconfig tap0 up
+ 
+sudo iptables -t nat -A POSTROUTING -o enp4s0 -j MASQUERADE
+sudo iptables -A FORWARD -i tap0 -j ACCEPT
+ 
+sudo sysctl -w net.ipv4.ip_forward=1
+ 
+sudo route add -net 10.0.0.0/8 gw 192.168.122.1 dev tap0
