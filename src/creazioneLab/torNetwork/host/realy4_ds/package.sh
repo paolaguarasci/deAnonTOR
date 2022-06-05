61a62925d665b6dd4b7e529d6a3991e209489cde
Paola Guarasci <paolaguarasci@gmail.com>
2022-06-05 23:00:26 +0200

    piccoli fix

--- src/creazioneLab/torNetwork/host/realy4_ds/package.sh
+++ src/creazioneLab/torNetwork/host/realy4_ds/package.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/bash
-sudo apt update 
-sudo apt upgrade
-sudo apt -y install tor apache2 php libapache2-mod-php
+sudo apt update && sudo apt upgrade -y && sudo apt -y install tor apache2 php libapache2-mod-php
