ae0a87e72d4ed8fd47611eada62d9e09364289f0
Paola Guarasci <paolaguarasci@gmail.com>
2022-06-13 21:14:12 +0200

    remove site

--- src/site/downloadSite.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-
-LINK=("facebook.it" "ansa.it" "unical.it" "google.it" "wikipedia.it")
-
-for i in "${LINK[@]}"
-do
-  echo "Downloading $i"
-  sudo wget -e robots=off --wait 1 -H -p -k http://$i
-done
